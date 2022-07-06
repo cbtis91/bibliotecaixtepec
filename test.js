84aeb54fd17055ec7023d26120f28714805474b9
EpicAlberto <edyal2222@gmail.com>
2022-07-05 22:58:13 -0500

    Implemento php desde un servidor externo

---
+++ test.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Al presionar el cuadro de color...
+    $(".cuadro").click(function() {
+        // ... se le pone el atributo src al iframe para que cargue la página
+        $("#php").attr("src","https://sitiofandom.000webhostapp.com/php/retorna5.php");
+    });
+});
+
+window.addEventListener('message', event => {
+    // Comprobamos si el mensaje viene de el sitio que queremos
+    if (event.origin.startsWith("https://sitiofandom.000webhostapp.com")) {
+        alert(event.data)
+    }
+})
